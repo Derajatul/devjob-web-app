2667e4622997c3326dc31f0ca55f42a3ac74f7af
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-17 11:41:29 +0000

    fix type error

--- app/contexts/ThemeContext.tsx
+++ app/contexts/ThemeContext.tsx
@@ -1,8 +1,12 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
+import { ReactNode } from 'react' // Import the ReactNode type
 
+interface ProvidersProps {
+  children: ReactNode; // Use ReactNode type for children prop
+}
 
-export function Providers({ children }) {
+export function Providers({ children }: ProvidersProps) {
   return <ThemeProvider>{children}</ThemeProvider>
 }
